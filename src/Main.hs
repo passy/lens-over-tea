d2166c6edbb74b63a17640abb8ee3a39822d00c2
Pascal Hartig <phartig@rdrei.net>
2016-03-30 15:48:01 +0200

    lens-1

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,46 @@
+{-# LANGUAGE RankNTypes    #-}
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
+import           Control.Applicative
+
+type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+type Lens' s a = Lens s s a a
+
+-- _1 :: Functor f => (a -> f b) -> (a, x) -> f (b, x)
+_1 :: Lens (a, x) (b, x) a b
+_1 = _
+
+-- _2 :: Functor f => (a -> f b) -> (x, a) -> f (x, b)
+_2 :: Lens (x, a) (x, b) a b
+_2 = _
+
+-- Make a lens out of a getter and a setter.
+lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
+lens get set = _
+
+-- Combine 2 lenses to make a lens which works on Either. (It's a good idea
+-- to try to use bimap for this, but it won't work, and you have to use
+-- explicit case-matching. Still a good idea, tho.)
+choosing :: Lens s1 t1 a b -> Lens s2 t2 a b
+         -> Lens (Either s1 s2) (Either t1 t2) a b
+choosing l1 l2 = _
+
+-- Modify the target of a lens and return the result. (Bonus points if you
+-- do it without lambdas and defining new functions. There's also a hint
+-- before the end of the section, so don't scroll if you don't want it.)
+(<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
+(<%~) l f s = _
+
+-- Modify the target of a lens, but return the old value.
+(<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
+(<<%~) l f s = _
+
+-- There's a () in every value. (No idea what this one is for, maybe it'll
+-- become clear later.)
+united :: Lens' s ()
+united = _
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = print "Hi"
