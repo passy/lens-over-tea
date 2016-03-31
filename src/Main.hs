97754aa6809e817f0e564b04a6d67939313cfdc1
Pascal Hartig <phartig@rdrei.net>
2016-03-31 13:33:02 +0200

    Modifications

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,7 @@
 module Main where
 
 import           Control.Applicative
+import           Control.Arrow         ((&&&))
 import           Data.Functor.Identity
 
 type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
@@ -37,16 +38,16 @@ choosing l1 l2 f = \case
 -- do it without lambdas and defining new functions. There's also a hint
 -- before the end of the section, so don't scroll if you don't want it.)
 (<%~) :: Lens s t a b -> (a -> b) -> s -> (b, t)
-(<%~) l f s = _
+(<%~) l f = l (f &&& f)
 
 -- Modify the target of a lens, but return the old value.
 (<<%~) :: Lens s t a b -> (a -> b) -> s -> (a, t)
-(<<%~) l f s = _
+(<<%~) l f = l (\a -> (a, f a))
 
 -- There's a () in every value. (No idea what this one is for, maybe it'll
 -- become clear later.)
 united :: Lens' s ()
-united = _
+united = lens (const ()) const
 
 view :: Lens s t a b -> s -> a
 view lens s = x
@@ -56,8 +57,25 @@ view lens s = x
 over :: Lens s t a b -> ((a -> b) -> s -> t)
 over l f = runIdentity . l (Identity . f)
 
+_abs :: Real a => Lens' a a
+_abs f n = update <$> f (abs n)
+  where
+    update x
+      | x < 0     = error "_abs: absolute value can't be negative"
+      | otherwise = signum n * x
+
+_all :: Eq a => a -> Lens' [a] a
+_all ref = lens get set
+  where
+    get s = ref
+    set s new = map (\old -> if old == ref then new else old) s
+
 main :: IO ()
 main = do
   print $ view _1 (1, 2)
   print $ view _2 (1, 2)
+  print $ view (_1 . _2) ((1, 4), 2)
   print $ over _1 (* 44) (1, 2)
+  print $ over _1 (* 44) (1, 2)
+  print $ over _abs (^ 2) (-10)
+  print $ over (_all 0) (const 1337) [100, 200, 0, 300, 0, 400]
