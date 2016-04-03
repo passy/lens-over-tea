f645f6f222014f34a438499fc8f0f75f9f938ec1
Pascal Hartig <phartig@rdrei.net>
2016-04-03 11:08:46 +0200

    Lens over tea part 2, presents

---
+++ src/Main3.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Main where
+
+import Control.Applicative
+import Data.Functor.Identity
+import Data.Functor.Compose
+
+type Traversal s t a b = forall f. Applicative f => (a -> f b) -> s -> f t
+type Traversal' s a = Traversal s s a a
+
+data Present = Wrapped Present | Toy
+  deriving (Show, Eq)
+
+unwrapped :: Traversal' Present Present
+unwrapped f (Wrapped x) = Wrapped <$> f x
+unwrapped f Toy = f Toy
+
+set' :: Present -> Identity Present
+set' t = Identity (Wrapped t)
+
+get' :: forall b. Present -> Const [Present] b
+get' t = Const [t]
+
+main :: IO ()
+main = do
+  print . fmap (unwrapped get') . unwrapped set' $ Toy
